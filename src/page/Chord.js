c7ffb5313851aba2df0b94379c04c44c40c92e60
tyNAKAMOL <beauty.nkm39@gmail.com>
2021-06-30 22:35:48 +0700

    new

--- src/page/Chord.js
+++ src/page/Chord.js
@@ -6,11 +6,18 @@ export default function Chord() {
   const Location = useLocation();
   const a = Location.params?.song;
   const flieName = "/chord/" + a + ".png";
+  const album = "/img1/" + a + ".jpg";
   return (
     <div className="container-pathchord ">
-      <div className="play-music">u</div>
+      <div className="play-music">
+        <div className="container-album">
+          <img className="img-bum" src={album} />
+          <p>{a} - Three Man Down</p>
+        </div>
+      </div>
+
       <div className="chord">
-        <img src={flieName} />
+        <img className="img-1" src={flieName} />
       </div>
       <div className="suggest-song">tyur</div>
       {/* <p>{a}</p> */}
